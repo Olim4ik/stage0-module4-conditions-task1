dd2c993c9a689504f7c1c765d8a6f1b3afcbeca2
Olim4ik <oakhmadjonov.uz@gmail.com>
2023-12-07 23:06:42 +0500

    test 2

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
@@ -2,6 +2,10 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalTruePrinter {
     public static void main(String[] args) {
-        System.out.println(true);
+        if (true) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
     }
 }
