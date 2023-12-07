2147eb0ce0b83cc826902400f4209cbcea810ca4
Olim4ik <oakhmadjonov.uz@gmail.com>
2023-12-07 22:57:13 +0500

    test 1

--- src/main/java/school/mjc/stage0/conditions/task1/PositiveNumberDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task1/PositiveNumberDeterminer.java
@@ -2,5 +2,6 @@ package school.mjc.stage0.conditions.task1;
 
 public class PositiveNumberDeterminer {
     public void isPositive(int numberToBeDetermined) {
+        System.out.println(numberToBeDetermined > 0);
     }
 }
